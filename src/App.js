daf15d81e593d6f09d2a3af402f284ba2e3a3322
BannyT <tumusiimebarnabus@gmail.com>
2021-08-13 14:48:51 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Sidebar  from './Sidebar';
 function App() {
   return (
     <div className="app">
          <div className="main-body">
+           <Sidebar/>
          </div>
     </div>
   );
---
+++ src/Sidebar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Sidebar.css"
+
+export default function Sidebar() {
+  return (
+    <div>
+        <h3>My side bar</h3>
+    </div>
+  )
+}
