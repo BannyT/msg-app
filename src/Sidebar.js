1b41ed10125674c71b76e0a9605182ab90999f92
BannyT <tumusiimebarnabus@gmail.com>
2021-08-14 12:33:09 +0300

    update

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -5,13 +5,13 @@ import { DonutLarge } from '@material-ui/icons'
 import ChatIcon from '@material-ui/icons/Chat';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import SearchIcon from '@material-ui/icons/Search';
-import SidebarChats from './SidebarChats';
+import AccountBoxIcon from '@material-ui/icons/AccountBox';
 
 export default function Sidebar() {
   return (
     <div className="side-bar">
           <div className="sidebar-header">
-
+            <Avatar/>
              <div className="header-right">
 
              </div>
