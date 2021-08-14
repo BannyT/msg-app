b8bf9f86dba8a9eebed69c75c56a919e01cd3978
BannyT <tumusiimebarnabus@gmail.com>
2021-08-14 12:08:10 +0300

    update

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,5 +1,11 @@
 import React from 'react'
 import "./Sidebar.css"
+import {Avatar, IconButton} from "@material-ui/core"
+import { DonutLarge } from '@material-ui/icons'
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import SearchIcon from '@material-ui/icons/Search';
+import SidebarChats from './SidebarChats';
 
 export default function Sidebar() {
   return (
